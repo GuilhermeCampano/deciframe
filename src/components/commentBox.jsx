d7566fa9f7be0f3fa3214b7a1f24a44ca5929343
Cameras <Cameras>
2016-10-02 16:21:27 -0300

    Permite adicionar 'comentario' que não é mais comentário

--- src/components/commentBox.jsx
+++ src/components/commentBox.jsx
@@ -4,29 +4,50 @@ import Axios from 'axios';
 import CommentForm from './commentForm';
 import CommentList from './commentList';
 
-
 class CommentBox extends React.Component {
   constructor(props) {
     super(props);
-    let self = this;
     this.state = {
       data: []
     };
-    Axios.get(props.url)
+    this.loadCommentsFromServer = this.loadCommentsFromServer.bind(this);
+    this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
+  }
+  loadCommentsFromServer(){
+    let self = this;
+    Axios.get(this.props.url)
     .then(function (response) {
-      console.log(response.data.results);
-      self.state.data = response.data.results;
+      self.setState({data:response.data.results});
     })
     .catch(function (error) {
       console.log(error);
     });
   }
+  handleCommentSubmit(comment) {
+    console.info('new comment', comment);
+    let newData = this.state.data;
+    newData.push({
+      title:comment.title,
+      producer:comment.producer,
+      created:new Date()
+    });
+    this.setState({data:newData});
+  }
+  componentDidMount() {
+    // this.loadCommentsFromServer();
+  }
   render() {
+    let commentList;
+    if (this.state.data.length > 0) {
+      commentList = <CommentList data={this.state.data}/>
+    } else {
+      commentList = <h2>Ops vazio</h2>;
+    }
     return (
       <div className="commentBox">
-        <CommentForm/>
+        <CommentForm onCommentSubmit={this.handleCommentSubmit} />
         <hr/>
-        <CommentList data={this.state.data}/>
+        {commentList}
       </div>
     )
   }
--- src/components/commentForm.jsx
+++ src/components/commentForm.jsx
@@ -2,16 +2,51 @@ import React from 'react';
 export default class CommentForm extends React.Component {
   constructor(props) {
    super(props);
+   this.state = {
+     title:'',
+     producer:''
+   }
    this.handleClick = this.handleClick.bind(this);
- }
+   this.handleTitleChange = this.handleTitleChange.bind(this);
+   this.handleProducerChange = this.handleProducerChange.bind(this);
+   this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleTitleChange(e) {
+   this.setState({title: e.target.value});
+  }
+  handleProducerChange(e) {
+   this.setState({producer: e.target.value});
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    var title = this.state.title.trim();
+    var producer = this.state.producer.trim();
+    if (!producer || !title) {
+      return;
+    }
+    this.props.onCommentSubmit({title: title, producer: producer});
+    this.setState({title: '', producer: ''});
+  }
   handleClick() {
    console.log(this); // React Component instance
   }
   render() {
     return (
-      <div className="commentForm" onClick={this.handleClick}>
-        Hello, world! I am a CommentFormmmmmm.
-      </div>
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+        {this.state.title} | {this.state.producer}
+        <br/>
+        <input type="text"
+          placeholder="title"
+          onChange={this.handleTitleChange}
+          value={this.state.title}
+        />
+        <input type="text"
+          placeholder="producer"
+          onChange={this.handleProducerChange}
+          value={this.state.producer}
+        />
+        <input type="submit" value="Post" />
+      </form>
     )
   }
 }
