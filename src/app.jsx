17e4e4db608443bb6ed06433c9a0746b8ec05cff
Guilherme Campano <guilherme.campano@concretesolutions.com.br>
2016-10-07 11:06:47 -0300

    Adiciona comportamento de rotas; Remove pages; Adicionas Menu

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,34 @@
 import styles from './index.scss';
 import React from 'react';
-import Pages from './components/pages/pages.jsx';
+import {render} from 'react-dom'
+import {Router, Route, Link, browserHistory} from 'react-router';
+import Home from './components/home/home.jsx';
+import About from './components/about/about.jsx';
+import Stages from './components/stages/stages.jsx';
+import TodoBox from './components/todo/todoBox.jsx';
+import Menu from './components/menu/menu.jsx';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+class Main extends React.Component {
   render() {
     return (
       <div className="container">
-        <Pages/>
+        <Menu/>
+        <div className="content">
+          {this.props.children}
+        </div>
       </div>
     )
   }
 }
+
+render((
+  <Router history={browserHistory}>
+    <Route path="/" component={Main} path="app">
+      <Route path="/" component={Home}></Route>
+      <Route path="/home" component={Home}></Route>
+      <Route path="/about" component={About}></Route>
+      <Route path="/todo" component={TodoBox}></Route>
+      <Route path="/stages" component={Stages}></Route>
+    </Route>
+  </Router>
+), document.getElementById('app'))
