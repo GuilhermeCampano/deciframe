7419d13c5b11cee4bbd64709a30803b74ee86501
Guilherme Campano <guilherme.campano@concretesolutions.com.br>
2016-10-17 11:18:29 -0200

    Add mobile menu

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import {Link} from 'react-router';
 export default class Menu extends React.Component {
+  componentDidMount = () => {
+    $(".button-collapse").sideNav();
+  }
   render() {
     return (
       <nav>
         <div className="nav-wrapper teal">
           <a className="brand-logo">Logo</a>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/todo">Todo</Link>
-            </li>
-            <li>
-              <Link to="/stages">Stages</Link>
-            </li>
-          </ul>
+          <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
+            <ul className="right hide-on-med-and-down">
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/todo">Todo</Link></li>
+              <li><Link to="/stages">Stages</Link></li>
+            </ul>
+            <ul className="side-nav" id="mobile-demo">
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/todo">Todo</Link></li>
+              <li><Link to="/stages">Stages</Link></li>
+            </ul>
         </div>
       </nav>
     )
